c35332bdea385531b0cb9e59c456e25c7a756b6e
ibrahima <ibrahimahotia.diallo@gmail.com>
2020-04-10 23:40:00 +0200

    Test du model

---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -0,0 +1,41 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Renvoie le Compte  en fonction de son numéro
+ * s'il est présent dans la liste
+ */
+class CompteComptableTest {
+
+    private static CompteComptable vCompte;
+    private static List<CompteComptable> vList;
+
+    @BeforeEach
+    void init() {
+        vCompte = new CompteComptable();
+        vCompte.setNumero(401);
+        vCompte.setLibelle("Fournisseurs");
+        vList = new ArrayList<>(0);
+        vList.add(vCompte);
+        vList.add(new CompteComptable(411, "Clients"));
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        vCompte = null;
+        vList.clear();
+    }
+
+    @Test
+    void getByNumero() {
+        assertEquals(CompteComptable.getByNumero(vList, 401), vCompte);
+    }
+}
